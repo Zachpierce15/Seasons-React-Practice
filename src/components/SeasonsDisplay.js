0f38057c98f3e795c59535d4b7cd85d21672fbd9
Zachary <zachary.15pierce@gmail.com>
2019-10-16 17:00:15 -0600

    Added Icons from the Semantics library to SeasonDisplay

--- src/components/SeasonsDisplay.js
+++ src/components/SeasonsDisplay.js
@@ -1,16 +1,22 @@
 import React from 'react'
 
-class SeasonDisplay extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {}
+const getSeason = (lat , month) => {
+  if(month > 2 && month < 9) {
+   return lat > 0 ? 'summer' : 'winter'
+  } else {
+   return lat > 0 ? 'winter' : 'summer'
   }
-  render() {
-    return (
-      <div>
-        Latitude: {this.props.lat}
-      </div>
-    )
-  }
+}
+
+const SeasonDisplay = props => {
+  const season = getSeason(props.lat, new Date().getMonth())
+  const icon = season === 'winter' ? 'snowflake' : 'sun'
+  return (
+    <div>
+      <i className={`massive ${icon} icon`} />
+      {season === 'winter' ? <h1>Burr, it is chilly</h1>: <h1>Lets hit the beach!</h1>} 
+      <i className={`massive ${icon} icon`} />
+    </div>
+  )
 }
 export default SeasonDisplay
